43fc9c4cb42f5c060352e6a4b4c97445391af123
AliBaydur <59739580+AliBaydur@github.com>
2022-01-25 22:35:50 +0300

    Add project files.

---
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Writer
+{
+    
+    public class WriterAboutOnDashboard : ViewComponent
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        public IViewComponentResult Invoke()
+        {
+            var usermail = User.Identity.Name;
+            Context c = new Context();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = wm.GetWriterById(writerID);
+            return View(values);
+        }
+    }
+}
